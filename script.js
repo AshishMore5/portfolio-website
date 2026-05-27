7ae3d5f33651095c848178f062f77e38a15eac9b
AshishMore5 <ashishsureshmore@gmail.com>
2026-05-27 15:03:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// VARIABLES
+
+let studentName = "Ashish";
+
+let course = "JavaScript Basics";
+
+// FUNCTION
+
+function showMessage() {
+
+    document.getElementById("message").innerHTML =
+        "Welcome " + studentName + "! You are learning " + course;
+
+}
